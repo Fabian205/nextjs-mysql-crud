e9afb96c2f91dc8e9daa534a49c6d2de9da99648
Fabian205 <fabaguilar205@gmail.com>
2023-04-13 20:54:49 -0500

    Connection with planetScale

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,31 @@
+// connection with planetScale
 import { createPool } from "mysql2/promise";
+
+const pool = createPool({
+  host: "aws.connect.psdb.cloud",
+  user: "d4m5u5dtjdhqpdsn139p",
+  password: "pscale_pw_BpbjBqVxzmFbP5wwIi9aG3Bo0EOoTLamU2ComyUelJa",
+  //port: 3306,
+  database: "productsdb",
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+console.log("conection ready");
+export { pool };
+
+
+
+
+//original local connection
+/* import { createPool } from "mysql2/promise";
 
 const pool = createPool({
   host: "localhost",
   user: "root",
-  password:"nobasys2411",
+  password: "nobasys2411",
   port: 3306,
   database: "productsdb",
 });
-
-export { pool };
+console.log("conection ready");
+export { pool }; */
