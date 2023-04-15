db0369fd987eab50534a8715e2c612d956e28dac
Fabian205 <fabaguilar205@gmail.com>
2023-04-15 04:45:43 -0500

    Config database for deploy

--- src/config/db.js
+++ src/config/db.js
@@ -1,20 +1,14 @@
-// connection with planetScale
+//Config para subir a railway
 import { createPool } from "mysql2/promise";
+import { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } from "./config.js";
 
-const pool = createPool({
-  host: "aws.connect.psdb.cloud",
-  user: "d4m5u5dtjdhqpdsn139p",
-  password: "pscale_pw_BpbjBqVxzmFbP5wwIi9aG3Bo0EOoTLamU2ComyUelJa",
-  //port: 3306,
-  database: "productsdb",
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-console.log("conection ready");
-export { pool };
-
-
+export const pool = createPool({
+  user: DB_USER,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  port: DB_PORT,
+  database:DB_NAME
+})
 
 
 //original local connection
@@ -26,6 +20,22 @@ const pool = createPool({
   password: "nobasys2411",
   port: 3306,
   database: "productsdb",
+});
+console.log("conection ready");
+export { pool }; */
+
+// connection with planetScale
+/* import { createPool } from "mysql2/promise";
+
+const pool = createPool({
+  host: "aws.connect.psdb.cloud",
+  user: "ymevsyxb4mlpibdissiv",
+  password: "pscale_pw_Iq3gkSJxzGPPlFdi26T5nc9ZFPTlResB8uIYxAojMfh",
+  //port: 3306,
+  database: "productsdb",
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 console.log("conection ready");
 export { pool }; */
