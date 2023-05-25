ead0e9b313cbac0aa978449421752aeed5ea6382
Fabian205 <fabaguilar205@gmail.com>
2023-05-25 08:57:43 -0500

    Product-Card css

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,15 +4,15 @@ export function ProductCard({ product }) {
   return (
     <Link legacyBehavior href={`/products/${product.id}`} key={product.id}>
       <a>
-        <div className="border border-gray-200 shadow-md p-6">
+        <div className="border border-gray-200 shadow-md p-3">
           <div className="flex flex-row space-x-4">
-            <h1 className="text-indigo-500">Account:</h1>
+            <h1 className="text-indigo-500">Cta:</h1>
             <h2>{product.name}</h2>
           </div>
 
-          <div className="flex flex-row space-x-4">
-            <p className="text-indigo-500">Description:</p>
-            <p>{product.description}</p>
+          <div className="flex flex-col">
+            <p className="text-indigo-500">Concepto:</p>
+            <p className="ml-3">{product.description}</p>
           </div>
 
           {/* <div className="flex flex-col">
@@ -21,7 +21,7 @@ export function ProductCard({ product }) {
           </div> */}
 
           <div className="flex flex-row space-x-4">
-            <p className="text-indigo-500">Price:</p>
+            <p className="text-indigo-500">Valor:</p>
             <p>{product.price}</p>
           </div>
         </div>
