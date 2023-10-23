2f94589b12d6d0bf08aef9830596e83d9d913138
Fabian205 <fabaguilar205@gmail.com>
2023-10-23 16:37:09 -0500

    Footer implement

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,55 +3,17 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://www.nobasys.com/"
-          target="_blank"
-          className="flex items-center"
-        >
-          <img
-            src="nobalog.png"
-            className="h-8 mr-5"
-            alt="www.nobasys.com"
-          />
-          {/* <span className="text-xs xs:text-xs sm:text-sm md:text-md lg:text-lg xl:text-xl  self-center font-semibold whitespace-nowrap dark:text-white">
-            Software Dev
-          </span> */}
-        </a>
-        {/* <button
-          id="boton"
-          onClick={boton}
-          data-collapse-toggle="navbardefault"
-          type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbardefault"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </button> */}
-        <div className="hidden w-full md:block md:w-auto" id="navbardefault">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
+    <>
+      <div className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <ul className="font-small flex flex-row p-2 md:p-0 mt-2 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            {/* <li>
               <Link legacyBehavior href="/">
                 <a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                   Init
                 </a>
               </Link>
-            </li>
+            </li> */}
             <li>
               <Link legacyBehavior href="/home">
                 <a
@@ -62,13 +24,13 @@ function Footer() {
                 </a>
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link legacyBehavior href="/about">
                 <a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                   About
                 </a>
               </Link>
-            </li>
+            </li> */}
             <li>
               <Link legacyBehavior href="/new">
                 <a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
@@ -90,7 +52,7 @@ function Footer() {
                 </a>
               </Link>
             </li>
-            <li>
+            {/* <li>
               <Link legacyBehavior href="/register">
                 <a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
                   Register
@@ -105,11 +67,11 @@ function Footer() {
                   Logout
                 </a>
               </Link>
-            </li>           
+            </li>  */}          
           </ul>
         </div>
       </div>
-    </nav>
+    </>
   );
 }
 
